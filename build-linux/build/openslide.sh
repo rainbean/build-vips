ef30a32013667b76cc4e0d19b52d1f0aa01fc262
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2023-11-16 12:11:35 +0800

    update openslide tag

--- build-linux/build/openslide.sh
+++ build-linux/build/openslide.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Build openslide"
-OPENSLIDE_TAG="2023-11-15"
+OPENSLIDE_TAG="2023-11-16"
 wget -q https://github.com/rainbean/openslide/archive/refs/tags/${OPENSLIDE_TAG}.tar.gz -O openslide.tar.gz
 tar xf openslide.tar.gz
 cd openslide-${OPENSLIDE_TAG}
