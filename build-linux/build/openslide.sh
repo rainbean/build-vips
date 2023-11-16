1773bef31146fe74b0de974b9914c906ba2ed8d0
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2023-11-16 08:50:23 +0800

    linux: revise openslide download url

--- build-linux/build/openslide.sh
+++ build-linux/build/openslide.sh
@@ -2,9 +2,10 @@
 
 echo "Build openslide"
 OPENSLIDE_TAG="2023-11-15"
-wget -q https://github.com/rainbean/openslide/releases/download/${OPENSLIDE_TAG}/openslide.tar.gz -O openslide.tar.gz
+https://github.com/rainbean/openslide/archive/refs/tags/2023-11-15.tar.gz
+wget -q https://github.com/rainbean/openslide/archive/refs/tags/${OPENSLIDE_TAG}.tar.gz -O openslide.tar.gz
 tar xf openslide.tar.gz
-cd openslide
+cd openslide-${OPENSLIDE_TAG}
 meson setup build --buildtype release
 meson compile -C build
 meson install -C build
