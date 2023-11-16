fbba3758310ae86a65570ef46c337a248fe672ee
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2023-11-16 08:54:54 +0800

    linux: fix bash syntax

--- build-linux/build/openslide.sh
+++ build-linux/build/openslide.sh
@@ -2,7 +2,6 @@
 
 echo "Build openslide"
 OPENSLIDE_TAG="2023-11-15"
-https://github.com/rainbean/openslide/archive/refs/tags/2023-11-15.tar.gz
 wget -q https://github.com/rainbean/openslide/archive/refs/tags/${OPENSLIDE_TAG}.tar.gz -O openslide.tar.gz
 tar xf openslide.tar.gz
 cd openslide-${OPENSLIDE_TAG}
