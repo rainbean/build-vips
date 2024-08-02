ad13be4330e19406f21ef9d61d4036a909c59b5e
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2024-08-02 15:02:06 +0800

    update openslide version to 2024-07-30

--- patch/openslide.mk
+++ patch/openslide.mk
@@ -2,11 +2,11 @@ PKG             := openslide
 $(PKG)_WEBSITE  := https://openslide.org/
 $(PKG)_DESCR    := C library for reading virtual slide images.
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2024-06-12
-$(PKG)_CHECKSUM := a02c6c94807a79db0865e5a327aff531aacfe2b9b6bb22a6324af1f4fec870f7
+$(PKG)_VERSION  := 2024-07-30
+$(PKG)_CHECKSUM := 15db174a6b2bdfd57846c34208daca8c88eab367023c21de6abab30b61444eb3
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
 $(PKG)_GH_CONF  := rainbean/openslide/tags
-$(PKG)_DEPS     := cc zlib glib libxml2 cairo gdk-pixbuf libjpeg-turbo tiff openjpeg sqlite libdicom
+$(PKG)_DEPS     := cc zlib glib libxml2 cairo gdk-pixbuf libjpeg-turbo tiff openjpeg sqlite libdicom zstd
 
 define $(PKG)_BUILD
     $(MXE_MESON_WRAPPER) \
