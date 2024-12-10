7a7f992fd2c0f144c0cfd7ab6df4e13aa7fd7134
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2024-12-10 10:23:38 +0800

    update openslide version to 2024-12-10

--- build-linux/build/openslide.sh
+++ build-linux/build/openslide.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Build openslide"
-OPENSLIDE_TAG="2024-11-21"
+OPENSLIDE_TAG="2024-12-10"
 wget -q https://github.com/rainbean/openslide/archive/refs/tags/${OPENSLIDE_TAG}.tar.gz -O openslide.tar.gz
 tar xf openslide.tar.gz
 cd openslide-${OPENSLIDE_TAG}
